e9192c534aa1b5d66bbb4d703eeeb4b11dbf186e
José Martínez Poblete <j.martinez.poblete@gmail.com>
2017-07-24 20:18:42 -0400

    Update TestUCE.java

--- TestUCE.java
+++ TestUCE.java
@@ -3,9 +3,9 @@ import javax.crypto.Cipher;
 
 public class TestUCE {
 	public static void main(String args[]) throws Exception {
-		  boolean unlimited = Cipher.getMaxAllowedKeyLength("RC5") >= 256;
-		  int maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
-      System.out.println("Java settings...");
+            boolean unlimited = Cipher.getMaxAllowedKeyLength("RC5") >= 256;
+            int maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
+            System.out.println("Java settings...");
 	    System.out.println("Unlimited cryptography enabled: " + unlimited);
 	    System.out.println("Maximum Key Length (AES): " + maxKeyLen);
 	}
