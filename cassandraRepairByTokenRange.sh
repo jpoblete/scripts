44864897b1c0d07a7ae54b98980f25698a55cf7d
José Martínez Poblete <j.martinez.poblete@gmail.com>
2017-12-15 12:49:47 -0500

    Updated doRepair()
    Corrected nodetool command to use JMX USR/PASSWD instead of Cassandra Credentials

--- cassandraRepairByTokenRange.sh
+++ cassandraRepairByTokenRange.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
-#
+###############################################################
 # Perform repair by local token range
 # Local ranges are broken into sub range repairs
+# Subrange repair: 
+# * IS NOT COMPATIBLE WITH INCREMENTAL REPAIRS
+# * AVOIDS ANTICOMPACTION after data is streamed 
+#
 # Assumptions:
 # * Cassandra is running
 # * The following is known
-#
+# 
 USR=cassandra
 PWD=cassandra
-#
+JMX_USR=""
+JMX_PWD=""
+###############################################################
 # CQLSH invoke
-#
+###############################################################
 TIMEOUT="300"
 CQLSH="cqlsh -u ${USR} -p ${PWD} --request-timeout=${TIMEOUT}"
-#
+###############################################################
 # Invoke Repair
-#
+###############################################################
 function doRepair(){
    KSP=$1
    TBL=$2
    STR=$3
    ETR=$4
-   nodetool -u ${USR} -pw ${PWD} repair -st ${STR} -et ${ETR} -- ${KSP} ${TBL} 
+   [ "${JMX_USR}" ] && [ "${JMX_USR}" ] && AUTH="-u ${JMX_USR} -pw ${JMX_PWD}"
+   nodetool ${AUTH} -pw ${JMX_PWD} repair -st ${STR} -et ${ETR} -- ${KSP} ${TBL} 
 }
-#
+###############################################################
 # Get Local Tokens
-#
+###############################################################
 function getTokens(){
    for i in $($CQLSH -e "select tokens from system.local;" | awk -F, '/{/{print $0}' | tr -d '{' | tr -d '}' | tr -d ','); do
        echo ${i//\'/}
    done | sort -n
 }
-#
-#
-#
+###############################################################
+# Calculate subranges, If STP>1 for even lower footprint
+# Otherwise repair the whole range 
+###############################################################
 function repairByTokenRange(){
    i=0
    STP=256
@@ -42,11 +50,7 @@ function repairByTokenRange(){
    tokens=($(getTokens))
    while [ ${i} -lt ${#tokens[@]} ]; do
          if [ "${tokens[i+1]}" ]; then
-            #
             # If STP > 1
-            # We are breaking into subranges
-            # Helps to have an even lower footprint
-            #
             if [ "${STP}" -gt 1 ]; then
                j=0
                range=$(echo "(${tokens[i+1]} - ${tokens[i]})" | bc -l)
@@ -60,10 +64,7 @@ function repairByTokenRange(){
                      doRepair ${KSP} ${TBL} ${subTokens[j]} ${subTokens[j+1]}
                      ((j++))
                done
-            #
             # Otherwise... STP=1
-            # We are repaiting the whole range
-            #
             else
                doRepair ${KSP} ${TBL} ${tokens[i]} ${tokens[i+1]}
             fi
@@ -71,9 +72,9 @@ function repairByTokenRange(){
          ((i++))
    done 
 }
-#
+###############################################################
 # Where all comes together
-#
+###############################################################
 main(){
    SCHEMA=$($CQLSH -e "describe schema;")
    echo "Begin processing ..."
@@ -85,9 +86,9 @@ main(){
    done    
    echo "End procesing"
 }
-#
+###############################################################
 # Execution
-#
+###############################################################
 main
 #
 #EOF
